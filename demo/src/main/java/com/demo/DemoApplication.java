b3b45d0ecf9b2efb0159a119b57f3e920323409e
Shreyas Jadhav <sshreyasjadhav2021@gmail.com>
2025-02-21 17:43:42 +0530

    Add files via upload

---
+++ demo/src/main/java/com/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/welcome")
+	public String home()
+	{
+		return "<h1>Welcome To Spring and Spring Boot Batcht</h1";
+	}
+
+}
