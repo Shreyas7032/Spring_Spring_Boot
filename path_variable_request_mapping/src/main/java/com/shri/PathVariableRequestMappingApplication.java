d6b5c739e5997290f9a3c918bc98d85f43b1cd75
Shreyas Jadhav <sshreyasjadhav2021@gmail.com>
2025-02-22 20:18:26 +0530

    Add files via upload

---
+++ path_variable_request_mapping/src/main/java/com/shri/PathVariableRequestMappingApplication.java
@@ -0,0 +1,13 @@
+package com.shri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathVariableRequestMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathVariableRequestMappingApplication.class, args);
+	}
+
+}
